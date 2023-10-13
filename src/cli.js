eb92e523593c30069d7f1cc151ff48e9b913fbfc
lauradefreitas2 <lauradovalegameplays@gmail.com>
2023-10-13 09:48:29 -0300

    organizando as pastas e criando os arquivos MD

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+// const {soma, lerArquivo} = require('../index.js');
+// const chalk = require('chalk')
+// const resultado = soma(1,3);
+// console.log(chalk.blue(resultado));
+
+// lerArquivo('./folder/arquivo1.md')
+// .then((conteudoDoArquivo)=> {
+//     console.log (chalk.bgGray(conteudoDoArquivo))
+// });
---
+++ src/mdLinks.js
@@ -0,0 +1,22 @@
+// const { Promises } = require("dns");
+// const fs = require("fs")
+
+// function soma (a, b) {
+//   return a + b
+
+// }
+
+// function lerArquivo(caminho) {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile(caminho, 'utf8', (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// }
+
+
+// module.exports = { soma, lerArquivo};
